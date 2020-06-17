0929322808fc136382a57f492dd951f9a7d3b293
Flavio Pinto <flaviopnt@hotmail.it>
2020-06-17 17:04:22 +0200

    Database and Model created

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,7 @@
+@include('partials.header')
+
+<main class="container">
+    @yield('main-content')
+</main>
+
+@include('partials.footer')
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css'/>
+        <title>Laravel CRUD</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand navbar-light bg-light">
+                <div class="nav-brand">Serie A</div>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
