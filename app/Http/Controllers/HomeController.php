9e06568188a2bd4bd5adfe96fc4eb8397f3dfe30
Flavio Pinto <flaviopnt@hotmail.it>
2020-06-17 17:25:42 +0200

    Get and print data from db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Team;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('home');
+        $teams = Team::all();
+        return view('home', compact('teams'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <h1>Homepage</h1>
+    <h1 class="mb-4">Homepage</h1>
+
+    <section class="teams">
+        <h2 class="text-primary">Clubs List</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Points</th>
+                    <th>Coach</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($teams as $team)
+                    <tr>
+                        <td>{{$team->name}}</td>
+                        <td>{{$team->city}}</td>
+                        <td>{{$team->points}}</td>
+                        <td>{{$team->coach}}</td>
+                        <td>{{$team->description}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </section>
 @endsection
