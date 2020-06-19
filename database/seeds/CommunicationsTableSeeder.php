ef4b827eaa0f955cdd2cc38a3e45b16e1fa346c3
Flavio Pinto <flaviopnt@hotmail.it>
2020-06-19 23:17:38 +0200

    Generation of fake data with Faker

--- database/seeds/CommunicationsTableSeeder.php
+++ database/seeds/CommunicationsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Communication;
+use Faker\Generator as Faker;
 
 class CommunicationsTableSeeder extends Seeder
 {
@@ -9,8 +11,50 @@ class CommunicationsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        //Metodo usando Faker
+        //Stabilisco numero di risultati da generare
+        $records = 10;
+        //Ciclo per aggiungere i dati alla tabella
+        for($i = 0; $i < $records; $i++) {
+            //Istanza
+            $newCommunication = new Communication();
+            $newCommunication->title = $faker->sentence(6, true);
+            $newCommunication->author = $faker->name();
+            $newCommunication->article = $faker->paragraph(3, true);
+            $newCommunication->save();
+        }
+
+        //Metodo senza usare faker:
+        //Creo array di array con i dati con cui voglio riempire la tabella (importante inserire use Students in alto)
+        // $communications = [
+        //     [
+        //         'title' => 'La Juve campione d\'Italia!',
+        //         'author' => 'Mino Taveri',
+        //         'article' => "La Juve è nella leggenda. Mai nessuno nella storia era riuscito a conquistare in Italia otto scudetti consecutivi, non c’è mai stata una squadra in grado di segnare un’epoca come hanno fatto i bianconeri dal 2012 in poi. Quest’anno non c’è mai stata discussione, la Juve ha dominato dall’inizio alla fine ed è riuscita a trionfare con cinque giornate di anticipo, un record come l’Inter del 2007. Merito di una campagna acquisti sontuosa che ad una squadra già fortissima ha aggiunto campione come Bonucci, Cancelo, Emre Can e soprattutto Cristiano Ronaldo."
+        //     ],
+        //     [
+        //         'title' => 'La Roma acquista Mancini!',
+        //         'author' => 'Nando Martellotti',
+        //         'article' => "Ora è ufficiale, Gianluca Mancini è un nuovo giocatore della Roma. Dopo aver sostenuto le visite mediche a Villa Stuart il difensore ex Atalanta ha firmato il contratto quinquennale a Trigoria insieme al direttore sportivo Petrachi. Questo il comunicato del club giallorosso."
+        //     ],
+        //     [
+        //         'title' => 'Il Napoli vince la Coppa Italia!',
+        //         'author' => 'Pierpiero Gallinetta',
+        //         'article' => "Il primo trofeo che è riuscito ad andare oltre la pandemia ha superato anche i 90' di gioco, fino ad arrivare all'appendice dei rigori. L'ha vinto il Napoli, che più della Juve aveva meritato, grazie all'infallibilità dei propri giocatori dal dischetto, ma anche alla parata di Meret sulla prima conclusione di Dybala. Un riscatto per il portierino del Napoli, reduce da una stagione tribolata. La maledizione di Sarri continua e si materializza una volta di più nella serata in cui Gattuso trionfa per la prima volta in carriera da allenatore, dopo avere vinto tutto da incontrista. Peccato che si sia trattato di una vittoria senza festa e senza boati di folla, senza il capo dello Stato e con l'inno steccato da Sergio Sylvestre, in un Olimpico vuoto e colorato solo virtualmente"
+        //     ],
+        //     [
+        //         'title' => 'Lorem Lorem Lorem!',
+        //         'author' => 'Sigmund Chiappaventi',
+        //         'article' => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis magnam sit illo reiciendis omnis in commodi, libero aspernatur unde soluta provident sunt, veritatis possimus eum tenetur expedita sequi quasi! Totam."
+        //     ]
+        // ];
+
+        // foreach($communications as $communication) {
+        //     $newCommunication = new Communication();
+        //     $newCommunication->fill($communication); //non serve il fillable perché il run lo disabilita e semplicemente prende i dati dall'array
+        //     $newCommunication->save();
+        // }
     }
 }
--- database/seeds/RefereesTableSeeder.php
+++ database/seeds/RefereesTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Referee;
+use Faker\Generator as Faker;
 
 class RefereesTableSeeder extends Seeder
 {
@@ -9,8 +11,64 @@ class RefereesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(Faker $faker)
+    {   
+        //Metodo usando Faker
+        //Stabilisco numero di risultati da generare
+        $records = 10;
+        //Ciclo per aggiungere i dati alla tabella
+        for($i = 0; $i < $records; $i++) {
+            //random gender
+            $gender = $faker->randomElement(['male', 'female']); //randomElement decide il genere in modo random
+            $genderShort = ($gender == 'male') ? 'm' : 'f';
+            //Istanza
+            $newReferee = new Referee();
+            $newReferee->name = $faker->name($gender);
+            $newReferee->city = 'Bologna';
+            $newReferee->gender = $genderShort;
+            $newReferee->date_of_birth = $faker->date($format = 'Y-m-d', $max = '1985-12-31');
+            $newReferee->description = $faker->paragraph(1, true);
+            $newReferee->save();
+        }
+
+
+        //Metodo senza usare faker (bisogna togliere il parametro Faker $faker):
+        //Creo array di array con i dati con cui voglio riempire la tabella (importante inserire use Students in alto)
+        // $referees = [
+        //     [
+        //         'name' => 'Gennaro Pierpaoli',
+        //         'city' => 'Bologna',
+        //         'gender' => 'm',
+        //         'date_of_birth' => '1978-03-12 10:37:05',
+        //         'description' => 'i pinguini sono animali molto carini'
+        //     ],
+        //     [
+        //         'name' => 'Tommaso Fasolo',
+        //         'city' => 'Milano',
+        //         'gender' => 'm',
+        //         'date_of_birth' => '1975-06-1 11:40:25',
+        //         'description' => 'i pinguini sono animali molto carini'
+        //     ],
+        //     [
+        //         'name' => 'Giovanna Loconsole',
+        //         'city' => 'Bari',
+        //         'gender' => 'f',
+        //         'date_of_birth' => '1980-12-1 20:37:05',
+        //         'description' => 'il re delle foreste illegali maialino uccisamente sapete selvatici in un a del!'
+        //     ],
+        //     [
+        //         'name' => 'Paolo Ferrante',
+        //         'city' => 'Ferrara',
+        //         'gender' => 'm',
+        //         'date_of_birth' => '1982-10-5 21:00:05',
+        //         'description' => 'sostiene che pippo baudo sia un grande statista'
+        //     ]
+        // ];
+
+        // foreach($referees as $referee) {
+        //     $newReferee = new Referee();
+        //     $newReferee->fill($referee); //non serve il fillable perché il run lo disabilita e semplicemente prende i dati dall'array
+        //     $newReferee->save();
+        // }
     }
 }
