c53f1adcb3ed645eaa2639829280a747b6e71647
Flavio Pinto <flaviopnt@hotmail.it>
2020-06-19 19:59:23 +0200

    2 new migration created

---
+++ database/migrations/2020_06_19_175415_create_communications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommunicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('communications', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 120);
+            $table->string('author', 40);
+            $table->mediumText('article');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('communications');
+    }
+}
